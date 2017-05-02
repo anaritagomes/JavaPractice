b4a2e088ba1ce237b664f2a5d1f69de7206d64bb
rita <anaritacruzgomes@gmail.com>
2017-05-02 23:26:22 +0100

    Testing if the hashes of the two objects are equal

--- Conditional Statement Exercises/src/designPatterns/SingletonPatternDemo.java
+++ Conditional Statement Exercises/src/designPatterns/SingletonPatternDemo.java
@@ -3,7 +3,24 @@ package designPatterns;
 public class SingletonPatternDemo {
 	
 	public static void main(String[] args) {
-		SingleObject obj = SingleObject.getInstance();
-		obj.showMessage();
+		SingleObject singleton1 = SingleObject.getInstance();
+		singleton1.showMessage();
+		SingleObject singleton2 = SingleObject.getInstance();
+		
+		// Since we're dealing with singleton pattern, the hashes must be equal
+		SingletonTest.getHash(singleton1);
+		SingletonTest.getHash(singleton2);
+		
 	}
+}
+
+
+ class SingletonTest { // We can't have two public classes in one file 
+/**
+ * Gets object hash.
+ * @param so
+ */
+	 static void getHash(SingleObject so) {
+		 System.out.println(so.hashCode());
+	 }
 }
